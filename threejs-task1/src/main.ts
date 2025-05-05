ea341721c9bc6d0a7fd8929f9ee322b94b32a2c3
Yuliya Kudrautsava <y.kudrautsava@andersenlab.com>
2025-05-05 23:38:07 +0200

    feat: set the camera

--- threejs-task1/src/main.ts
+++ threejs-task1/src/main.ts
@@ -1,6 +1,3 @@
-import { AxesHelper } from 'three';
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
-
 import './style.css';
 
 import { loadPlayerModel, player } from './objects/player/player';
@@ -9,29 +6,33 @@ import { camera } from './base/camera';
 import { scene } from './base/scene';
 import { clouds } from './objects/static/cloud';
 import { bushes } from './objects/static/bush';
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 0, 0);
-controls.update();
+import { coins } from './objects/coin/coin';
 
 // CLOUDS
 clouds.distributeClouds(30, 300, [60, 100]);
 
 // BUSHES
-bushes.distributeBushes(200, 300);
+bushes.distributeBushes(300, 300);
+
+// COIN
+coins.generateNewCoins(10, 300, [player.group, bushes.group]);
 
 // ADD PLAYER
 async function init() {
   await loadPlayerModel();
-  scene.add(player.group);
+  scene.add(player.group, camera);
 }
 init();
-scene.add(new AxesHelper(5));
 
 function animate() {
-  controls.update();
   player.render();
+  coins.render();
+  
   renderer.render(scene, camera);
+
+  camera.position.x = player.group.position.x;
+  camera.position.y = player.group.position.y - 6;
+  camera.position.z = player.group.position.z + 2;
 }
 renderer.setAnimationLoop(animate);
 
